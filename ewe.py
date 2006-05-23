0b3e1c211735ef2b1bf1948e88c0315a3dfe7dcf
Igor Serebryany <igor47@moomers.org>
2006-05-23 02:38:59 +0000

    basic server functioning

--- ewe.py
+++ ewe.py
@@ -1,98 +1,91 @@
 #!/usr/bin/python
 
 import sys,os
-import socket,ConfigParser
+import socket
 import logger,processors
 
-def generateConfig():
-	filename = raw_input("What do you want to name your config file?")
-	try:
-		fd = open(filename,"w")
-	except IOError, e:
-		print "Unable to create file: ",e
-		sys.exit(5)
-	
-	fd.write(configfile)
-	fd.flush()
-	fd.close()
-	print "Configuration file written"
+config = {
+"concurrency":"none",	#default concurrency model
+"port":4000,			#default port number
 
-def readConfigFile():
-	if len(sys.argv) != 2:
-		print "Usage: "+sys.argv[0]+"[<configuration file>|-n]"
+"poolthreads":5, #if preforking, how many threads to create
+"indexes":False, 	#generate directory indexes?
+
+"loglevel":0,	#log verbosity (0 = no logfile, 3 = maximum logging)
+"logfile":"ewe.log", #where to put the log
+
+"defaultindex":"index.html",	#the default file to open in a directory
+"documentroot":"."				#the location of the / url
+}
+
+def parseArguments():
+	if len(sys.argv) == 1:
+		return
+
+	class optionError(Exception):
+		pass
+
+	try:
+		if len(sys.argv) > 3:
+			raise optionError, "Too many arguments"
+		
+		for arg in sys.argv[1:]:
+			if arg in ("-f","-t","-p"): 
+				if arg == '-f':
+					config["concurrency"] = "forked"
+				elif arg == '-t':
+					config["concurrency"] = "threaded"
+				else:
+					config["concurrency"] = "threadpool"
+			else:
+				try:
+					config["port"] = int(arg)
+				except ValueError:
+					raise optionError, "Invalid argument " + arg
+	except optionError, e:
+		print "Error parsing arguments:", e
+		print "Syntax: %s [-f|-t|-p] [port]" % (sys.argv[0])
 		sys.exit(1)
-	
-	filename = sys.argv[1]
-	if filename == '-n':
-		generateConfig()
-		sys.exit(0)
-	
-	if not os.access(filename,os.R_OK):
-		print "Cannot read file " + filename + " - please make sure that\
-		the file exists and that you have appropriate permissions to read it."
-		sys.exit(2)
-	
-	opts = ConfigParser.ConfigParser()
-	try:
-		opts.read(filename)
-	except ConfigParser.ParsingError:
-		print "Configuration file " + filename + " could not be parsed.\
-				Try creating a new one with the -n option"
-		sys.exit(3)
-	else:
-		if not (opts.has_section("files and paths")	
-				or opts.has_section("server features")):
-			print "Required sections are missing from the config file.\
-					Try creating a new one with the -n option"
-			sys.exit(4)
-		return opts
 
-def buildLogger(opts):
-	logfile=opts.get("files and paths","logfile")
-	loglevel=opts.get("server features","loglevel")	
-	
-	logThread = logger.logThread(loglevel,logfile)
+def buildLogger():
+	logThread = logger.logThread(config["loglevel"],config["logfile"])
 	logThread.start()
 	return logThread
 	
-def initProcessor(opts,logger):
-	concurrency = opts.get("server features","concurency")
-
-	if concurrency == "none":
-		processor = processors.basic(logger)
-	elif concurrency == "processes":
-		processor = processors.forked(logger)
+def initProcessor(logger):
 	
-	processor.indexes = opts.getboolean("server features", "indexes")
-	processor.defaultindex = opts.get("files and paths", "defaultindex")
-	processor.documentroot = opts.get("files and paths", "document root")
+	procTypes = {"none":processors.basic,
+				"forked":processors.forked
+				}
+	processor = procTypes[config["concurrency"]](logger)
+	
+	processor.indexes = config["indexes"]
+	processor.defaultindex = config["defaultindex"]
+	processor.documentroot = config["documentroot"]
 
 	return processor
 	
-def serveRequests(processor,port):
+def serveRequests(processor):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind(('',port))
+	sock.bind(('',config["port"]))
 	sock.listen(5)
 	while True:
 		con,address = sock.accept()
 		processor.process(con,address)
 	
 def main():
-	opts = readConfigFile()
 	try:
 		try:
-			logger = buildLogger(opts)
+			logger = None
+			logger = buildLogger()
 			try:
-				processor = initProcessor(opts,logger)
-				serveRequests(opts,processor)
+				processor = None
+				processor = initProcessor(logger)
+				serveRequests(processor)
 			finally:
 				if processor: processor.quit()
 		finally:
 			if logger: logger.quit()
-	except ConfigParser.NoOptionError, e:
-		print e + "\nOption missing.  Add it or generate a new config file\
-					using the -n switch"
-		exit(7)
 
 	except socket.error, e:
 		if len(e) == 2:
@@ -100,34 +93,10 @@ def main():
 		else:
 			print "Socket Error: %s ... Quiting" % (e)
 		sys.exit(8)
-		
 	except KeyboardInterrupt:
 		print "Interrupt caught ... Quiting"
 		sys.exit(9)
-			
-	exit(0)
 	
-configfile = """\
-[Server Features]
-#   which concurency model should the server use? valid models are 
-#	none, processes, threads and threadpool (default: none)
-concurency = none
-#   if concurency is threadpool, how many threads should the server pre-fork? (default: 5)
-poolthreads = 5
-#    generate directory indexes? (default: no)
-indexes = no
-#    log verbosity: (0 = no logfile, 3 = max verbosity)
-loglevel = 0
-
-[Files And Paths]
-#    what is the path to the '/' URI? (default: . )
-documentroot = .
-#    where should the log be written? (default: ewe.log)
-logfile = ewe.log
-#    what should the server return if it recieves a request for a directory (default: index.html)
-defaultindex = index.html
-"""
-
 if __name__ == "__main__":
 	main()
 	sys.exit(0)
